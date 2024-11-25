ff659d4b7f6fe87fbd6a08b105e102febae7bf54
AmariGray <57196205+AmariGray@users.noreply.github.com>
2024-11-25 00:03:56 -0500

    Update Lab11.sql
    Ones that worked

--- Lab11.sql
+++ Lab11.sql
@@ -1,1 +1,44 @@
 
+--Amari 
+SELECT  course_id, semester, lab_time
+FROM courses
+
+
+--Amari 
+SELECT 'COMP1235'
+
+
+--Amari 
+ SELECT title, due_date
+ FROM assignments
+
+--Amari 
+ SELECT Min (due_date)
+ FROM assignments
+
+--Amari 
+ SELECT title, due_date, 'COMP1168'
+ FROM assignments 
+
+--Amari 
+ SELECT title, due_date
+ FROM assignments 
+Where '2024-10%'
+
+--Amari 
+ SELECT due_date
+ FROM assignments 
+Where status = 'Completed'
+
+
+
+--Amari 
+SELECT status, course_id, due_date 
+FROM assignments
+
+--Amari 
+ SELECT title, course_id
+ FROM assignments 
+WHERE due_date = 2024-10-08
+
+
