cbc98e8b3eae5b3d6edc2f28e67902e15fbf443d
AmariGray <57196205+AmariGray@users.noreply.github.com>
2024-11-25 00:28:21 -0500

    Update Lab12.sql

--- Lab12.sql
+++ Lab12.sql
@@ -1,1 +1,12 @@
 
+--Amari 
+SELECT concat(course_id, '-', semester) 
+From courses
+
+--Amari 
+SELECT course_id, course_name, lab_time, date('Fri') FROM courses
+-- showed up as NULL for "Fri" and "Friday"
+
+--Amari 
+SELECT * FROM assignments
+WHERE due_date < '2024-10-05'
