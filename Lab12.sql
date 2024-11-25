86199ad76f5f2140ab74193ec18bd994b1dbf225
AmariGray <57196205+AmariGray@users.noreply.github.com>
2024-11-25 00:40:13 -0500

    Update Lab12.sql

--- Lab12.sql
+++ Lab12.sql
@@ -10,3 +10,20 @@ SELECT course_id, course_name, lab_time, date('Fri') FROM courses
 --Amari 
 SELECT * FROM assignments
 WHERE due_date < '2024-10-05'
+
+--Amari 
+SELECT count(*) 
+FROM assignments
+GROUP BY status;
+
+--Amari 
+SELECT count(status) 
+FROM assignments
+GROUP BY status;
+
+--Amari 
+SELECT count() 
+FROM assignments
+GROUP BY status;
+-- couldn't get it to show the name of each status title "Not Started", "In Progress", "Completed"
+
