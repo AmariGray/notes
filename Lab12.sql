f86808e71de3ccda38354cb07e99c2e40c4b9477
AmariGray <57196205+AmariGray@users.noreply.github.com>
2024-11-25 01:21:27 -0500

    Update Lab12.sql

--- Lab12.sql
+++ Lab12.sql
@@ -1,29 +1,45 @@
 
---Amari 
+
 SELECT concat(course_id, '-', semester) 
 From courses
 
---Amari 
+ 
 SELECT course_id, course_name, lab_time, date('Fri') FROM courses
 -- showed up as NULL for "Fri" and "Friday"
 
---Amari 
+
 SELECT * FROM assignments
 WHERE due_date < '2024-10-05'
 
---Amari 
+
 SELECT count(*) 
 FROM assignments
 GROUP BY status;
-
---Amari 
+ 
 SELECT count(status) 
 FROM assignments
 GROUP BY status;
 
---Amari 
+
 SELECT count() 
 FROM assignments
 GROUP BY status;
 -- couldn't get it to show the name of each status title "Not Started", "In Progress", "Completed"
+
+ 
+SELECT course_name FROM courses
+ORDER By course_name DESC
+-- does not work as intendedassignments
+
+
+
+SELECT length(course_name) FROM courses
+ORDER By course_name DESC
+--shows by number
+
+--Amari 
+SELECT upper(course_name) FROM courses
+
+
+
 
